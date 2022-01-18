3a3552a86f048a4f6908f690eaac87ecd5733d01
javad290 <javad290@gmail.com>
2022-01-18 09:09:49 +0100

    .

--- script_01.js
+++ script_01.js
@@ -2,3 +2,36 @@
 
 // Entscheidungsstrukturen | control flow
 
+const ageJohn = 35
+const ageMark = 30
+
+//Deklaration
+
+let isJohnOlder, isJohnEqual
+
+// Test
+
+isJohnOlder = (ageJohn > ageMark)
+isJohnEqual = (ageJohn == ageMark)
+
+
+//Ausgabe
+
+console.log("ageJohn: " + ageJohn);
+console.log("ageJohn: " + ageMark);
+console.log("isJohnOlder " + isJohnOlder );
+console.log("isJohnEqual " + isJohnEqual );
+
+// **********************IF*************
+// TINA -->
+
+
+// if(false)
+// if(true
+// if(isJohnOlder)
+if(ageJohn > ageMark)
+{
+
+    console.log("John is älter.");
+
+}
