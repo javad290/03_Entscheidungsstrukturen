66dbdfdc2d192a5d4cd769f8be3321d005aef5bc
javad290 <javad290@gmail.com>
2022-01-18 11:50:22 +0100

    if else und swich case

--- script_01.js
+++ script_01.js
@@ -2,25 +2,25 @@
 
 // Entscheidungsstrukturen | control flow
 
-const ageJohn = 35
-const ageMark = 30
+// const ageJohn = 30
+// const ageMark = 30
 
-//Deklaration
+// //Deklaration
 
-let isJohnOlder, isJohnEqual
+// let isJohnOlder, isJohnEqual
 
-// Test
+// // Test
 
-isJohnOlder = (ageJohn > ageMark)
-isJohnEqual = (ageJohn == ageMark)
+// isJohnOlder = (ageJohn > ageMark)
+// isJohnEqual = (ageJohn == ageMark)
 
 
-//Ausgabe
+// //Ausgabe
 
-console.log("ageJohn: " + ageJohn);
-console.log("ageJohn: " + ageMark);
-console.log("isJohnOlder " + isJohnOlder );
-console.log("isJohnEqual " + isJohnEqual );
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageJohn: " + ageMark);
+// console.log("isJohnOlder " + isJohnOlder);
+// console.log("isJohnEqual " + isJohnEqual);
 
 // **********************IF*************
 // TINA -->
@@ -29,9 +29,83 @@ console.log("isJohnEqual " + isJohnEqual );
 // if(false)
 // if(true
 // if(isJohnOlder)
-if(ageJohn > ageMark)
-{
+// if(ageJohn > ageMark)
+// {
 
-    console.log("John is älter.");
+//     console.log("John is älter.");
 
+// }
+
+//************IF-Else ******/
+// mit Alternative
+//entweder ja oder nein und mehrere
+
+// if (isJohnOlder) {
+//     //Ja-Zweig /true
+//     console.log("John is älter.")
+
+// }
+// else if (isJohnEqual) {
+//     console.log("John is gleich alt.")
+// }
+
+// else {
+//     //Nein-Zweig /false
+//     console.log("John is Jünger.");
+
+// }
+
+
+// ****************CASE und Switch**********
+
+// const firstName = "Jane"
+// let job
+// job = "driver"      //fährt Taxi
+// job = "diver"       //taucht
+// job = "artist"      //malt ein Bild
+// job = "pilot"       // macht etwas anderes - - > default
+// job = "teacher"     //unterrichtet
+// job = "instructor"     //unterrichtet
+
+
+// switch (job) {
+//     case "driver":
+//         console.log(firstName + " fährt Taxi");
+//         break;
+//     case "diver":
+//         console.log(firstName + " diver");
+//         break;
+//     case "artist":
+//         console.log(firstName + " malt ein Bild");
+//         break;
+
+//     case "teacher":    //oder
+//     case "instructor":
+//         console.log(firstName + " unterrichtet");
+//         break;
+
+//     default: //wenn garnix trrift
+//         console.log(firstName + " macht etwas anderes");
+//         break;
+// }
+
+
+// ********************Case switch 2*********************
+
+const cond = true
+const a = 2
+
+
+switch (cond) {
+    case (a == 1):
+        console.log("a ist glech 1");
+
+        break;
+    case (a == 2):
+        console.log("a ist glech 2");
+        break;
+
+    default:
+        console.log("a hat einen anderen Wert");
+        break;
 }
